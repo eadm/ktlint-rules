0f968b3019d1a84c4d0cfba6a1d3cecc59a81f93
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-01-19 23:50:05 +0300

    add rules

---
+++ ktlint_rules/src/main/java/ru/nobird/android/ktlint/rules/NobirdRuleSetProvider.kt
@@ -0,0 +1,14 @@
+package ru.nobird.android.ktlint.rules
+
+import com.pinterest.ktlint.core.RuleSet
+import com.pinterest.ktlint.core.RuleSetProvider
+
+class NobirdRuleSetProvider : RuleSetProvider {
+    override fun get(): RuleSet =
+        RuleSet(
+            "nobird-ktlint-rules",
+            ExpressionBodyRule(),
+            ExpressionBodyIndentRule(),
+            ExplicitPublicTypeRule()
+        )
+}
